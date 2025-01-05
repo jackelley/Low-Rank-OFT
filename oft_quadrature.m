ddff81b275eef6c058b314e5382c9423054aef19
Jack Kelley <71041455+jackelley@users.noreply.github.com>
2025-01-05 16:18:21 -0500

    Update oft_quadrature.m

--- oft_quadrature.m
+++ oft_quadrature.m
@@ -1,11 +1,12 @@
 clear; clc; close all;
 % Time
 start_time = 0;
-dt_initial = 0.01;
+dt_initial = 0.005;
 dt_end = 10 * dt_initial;
 
 L = 2;
 kappa = 10;
+a0 = 10;
 T = kappa * L;
 
 % Get times, n_t, and constants
@@ -19,10 +20,10 @@ for i = 1:n_t - 1
 end
 
 % Spatial
-n_x = 100;
+n_x = 200;
 x = linspace(-1, 1, n_x);
 
-g = exp(-10 * abs(x).^2);
+g = exp(-a0 * abs(x).^2 + 1i * kappa * x(1));
 
 W1 = w1(times(1:end-1), times(2:end));
 W2 = w2(times(1:end-1), times(2:end));
@@ -43,3 +44,10 @@ end
 I = I / sqrt(-1i / pi);
 
 plot(x, real(I))
+
+e = ones(n_x, 1);
+A = (1/(kappa^2)) * spdiags([e -2*e e], -1:1, n_x, n_x);
+C = (speye(n_x, n_x) + A)^(1/2);
+v_exact = (speye(n_x, n_x) + A)^(1/2) \ g';
+
+relative_error = norm(v_exact - I, 'inf') / norm(v_exact', 'inf');
